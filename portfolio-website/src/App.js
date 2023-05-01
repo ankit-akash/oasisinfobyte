ab68b0e9080e8fbdbe26ca8f0bbd1ba28d05558b
“[Ankit <[akashankit905@gmail.com]>
2023-05-01 16:52:41 +0530

    committed

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <div className="p-[20px]">
+      {/* Nav Bar*/}
+      <NavBar />
+      {/* Intro*/}
+      {/* About me*/}
+      {/*Skills */}
+      {/* Portfolio*/}
+      {/* Companies*/}
+      {/* Contact us*/}
+      {/* Footer*/}
+    </div>
+  );
+}
+
+export default App;
